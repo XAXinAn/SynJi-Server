108b63f0f00fbe734d1a916c76d74b38f103d622
Jiang Hangxi <jianghangximail@gmail.com>
2026-01-23 19:37:27 +0800

    初始文件

---
+++ src/test/java/org/example/synjiserver/SynJiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.synjiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynJiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
